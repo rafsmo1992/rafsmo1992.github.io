9200fe4adf16a2cecbe53a8e63996e41f34a9cfa
Rafal Smolaga <rafsmo1992@gmail.com>
2023-01-12 23:26:13 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://kodilla-tasks-prod-kodilla-tasks-v8y52b.mo2.mogenius.io//v1/tasks';
-  const trelloApiRoot = 'https://kodilla-tasks-prod-kodilla-tasks-v8y52b.mo2.mogenius.io//v1/trello';
+  const apiRoot = 'https://kodilla-tasks-prod-kodilla-tasks-v8y52b.mo2.mogenius.io/v1/tasks';
+  const trelloApiRoot = 'https://kodilla-tasks-prod-kodilla-tasks-v8y52b.mo2.mogenius.io/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
